64cb37103b2ff8292f69c4bdc8bba3711dfaad7f
IVShvykin <IVShvykin@gmail.com>
2020-08-03 10:46:14 +0300

    xml to html converter

---
+++ Common/Helpers/ApiHelper/IXmlHtmlConverterHelper.cs
@@ -0,0 +1,7 @@
+﻿namespace Common.Helpers.ApiHelper
+{
+    public interface IXmlHtmlConverterHelper
+    {
+        string TransformXMLToHTML(string inputXml, string xsltString);
+    }
+}
---
+++ Common/Helpers/ApiHelper/XmlHtmlConverterHelper.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Xml;
+using System.Xml.Xsl;
+
+namespace Common.Helpers.ApiHelper
+{
+    public class XmlHtmlConverterHelper : IXmlHtmlConverterHelper
+    {
+        public string TransformXMLToHTML(string inputXml, string xsltString)
+        {
+            XslCompiledTransform transform = new XslCompiledTransform();
+            using (XmlReader reader = XmlReader.Create(new StringReader(xsltString)))
+            {
+                transform.Load(reader);
+            }
+            StringWriter results = new StringWriter();
+            using (XmlReader reader = XmlReader.Create(new StringReader(inputXml)))
+            {
+                transform.Transform(reader, null, results);
+            }
+            return results.ToString();
+        }
+    }
+}
